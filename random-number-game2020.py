69428b1015c6bfc4b7fd76f5d0869a7f2a910ea8
knusperflocky <manuela.oregon@gmail.com>
2020-09-18 17:08:12 -0700

    creating number game

---
+++ random-number-game2020.py
@@ -0,0 +1,20 @@
+# This is a random number guessing game
+import random
+print('Huhu, wie heisst Du denn?')
+name = input()
+print('Super, ' + name + ', I denke gerade an eine Zahl zwischen 1 und 20')
+geheimZahl = random.randint(1, 20)
+for anzahlRaten in range(1, 7):
+    print("Bitte rate mal.")
+    geraten = int(input())
+    if geraten < geheimZahl:
+        print('Meine Zahl ist groesser')
+    elif geraten > geheimZahl:
+        print('Meine Zahl ist kleiner')
+    else:
+        break # will break for correct guess
+if geraten == geheimZahl:
+    print('Super, ' + name + ', das war genau meine Zahl! Du hast sie mit ' + str(anzahlRaten) + ' Versuchen erraten!')
+else:
+    print('Nein, die Zahl an die ich dachte war ' + str(geheimZahl))
+
